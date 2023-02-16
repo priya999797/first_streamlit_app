fda82aa139d2daf041f4315ad0f20368b0b3da59
priya999797 <125552727+priya999797@users.noreply.github.com>
2023-02-16 15:18:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and bread')
+streamlit.text('Spinach and salad')
+streamlit.text('Hard boiled free range egg')
