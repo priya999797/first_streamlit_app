398bff7ed45201ed95a0b383adc723d5b533d6b4
priya999797 <125552727+priya999797@users.noreply.github.com>
2023-02-17 21:53:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ new_fruit = streamlit.text_input('What fruit would you like to add?')
 # my_fruit_list = my_fruit_list.append(new_fruit)
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-my_fruit_list = ['a', 'b', 'c']
+my_fruit_list = ['a', 'b', 'c', 'Avocado','Strawberries']
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
